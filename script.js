e55ca3a3ed74772ead9b683c14a5fdba76efc7e3
Kapowlo <rsmike91@outlook.com>
2024-12-04 18:05:29 -0500

    Selected the share svg image and container
    Added an event for share svg image
    
    Added variable to store screen width
    useful to pass that variable to a determineScreenSize(arg) to detect
    if user has a big enough screen or not
    
    Added function to determineScreenSize if user has small or big screen
    conditional test will return true if screen is big enough or false if too small
    
    Added acrticlePreviewActiveState() function
    by calling determineScreenSize(screenWidth) i'll get a true or false val
    first conditional test applies to mobile/smaller screen so if
    determineScreenSize(screenWidth) returns false the if block will run
    otherwise else code bloc runs  when true is returned from determineScreenSize(screenWidth)

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
+const body = document.body;
 
-console.log('hello world');
-
-
-/* 
-mobile version
-
+/*
+ no vertical scroll bar on my webpage i'll use this instead of window.innerWidth, 
+i want this variable so i can pass it as an arg to determineScreenSize func
 */
+const screenWidth =body.clientWidth; 
+
+const shareSvgImg = document.querySelector('.share-icon');
+const shareSvgContainer = document.querySelector('.share-icon-container');
 
 
-/* 
-desktop version
-*/
+
+function determineScreenSize(vw){
+  /* when this func is called it will return true or false based on user screen size
+    if it returns true then desktop styles will be aplied to share svg img otherwise apply mobile styles to share svg img
+  */
+  if(vw >= 1000){
+   
+    return true
+  }
+  else{
+    return false
+    
+  }
+}
+
+function acrticlePreviewActiveState(){
+  // will contain code for the sharesvgimg event for both mobile and desktop
+  // will temporarily contain code to append those social media icons to the container, but func will probly get too big so will move that code out 
+  
+  if(!determineScreenSize(screenWidth)){
+    console.log(`your screen is small it is currently ${screenWidth}px \napplying mobile layout styles`);
+    shareSvgImg.addEventListener("click",(e)=>{
+      console.log(`You clicked on this element: ${e.target.outerHTML}`,shareSvgImg);
+      
+    })
+  }
+  else{
+    console.log(`your screen is big it is currently ${screenWidth}px  \napplying desktop layout styles`);
+     shareSvgImg.addEventListener('click',(e)=>{
+      console.log(`You clicked on this element: ${e.target.outerHTML}`,shareSvgImg);
+    })
+  }
+  
+}
+
+acrticlePreviewActiveState();
