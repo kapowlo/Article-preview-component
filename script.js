e17fc9a0ca2deb8092db37b67fc97cbe26174dca
Kapowlo <rsmike91@outlook.com>
2025-01-06 16:34:41 -0500

    Added a resize event to window object passed a reference of func handleResize so when the resize event is triggered then the handleResize func executes inside handleResize func, screenwidth variable will store the value of body.clientWidth this fixed a bug where user would change screen size and the screenwidth variable would not change, ie: userScreen < 1000px but then user would increase his screen to be userScreen > 1000px and the mobile layout would be applied instead of the pc layout
    Removed everything inside acrticlePreviewActiveState()
    
    Added an event to shareSvgImgimg
    and moved the code from acrticlePreviewActiveState inside of event
    now whenever user clicks on the share svg img, it will check user screen size
    and apply correct layout.
    
    then it will check if the container of the share svg img has the class hidden
    and it will toggle it on/off
    
    note:see style.css file for all hidden-mobile styles

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const body = document.body;
  no vertical scroll bar on my webpage i'll use this instead of window.innerWidth, 
 i want this variable so i can pass it as an arg to determineScreenSize func
 */
-const screenWidth =body.clientWidth; 
+let screenWidth =body.clientWidth; 
 
 const shareSvgImg = document.querySelector('.share-icon');
 const shareSvgContainer = document.querySelector('.share-icon-container');
@@ -15,7 +15,15 @@ const jsShareIcon2 = document.getElementById('js-share-icon-2');
 const jsHiddenMobile = document.getElementById('js-hidden-mobile');
 const jsHiddenContainer =document.getElementById('js-hidden-container');
 
+/*this resize event listener i added to window object, will update screenWidth variable when window size is modified 
+fixes issue where after changing screen size from mobile to pc the screenwidth variable would'nt update correctly
+*/
+window.addEventListener('resize',handleResize);
 
+function handleResize(){
+  screenWidth=body.clientWidth;
+  return screenWidth;
+}
 
 
 function determineScreenSize(vw){
@@ -23,55 +31,57 @@ function determineScreenSize(vw){
     if it returns true then desktop styles will be aplied to share svg img otherwise apply mobile styles to share svg img
   */
   if(vw >= 1000){
-   
+    console.log(`applying pc layout your viewport width is :${screenWidth}px`);
+    
     return true
   }
   else{
+    console.log(`applying mobile layout your viewport width is :${screenWidth}px`);
+    
     return false
     
   }
 }
 
-function acrticlePreviewActiveState(){
-  // will contain code for the sharesvgimg event for both mobile and desktop
-  // will temporarily contain code to append those social media icons to the container, but func will probly get too big so will move that code out 
+
+
+/*
+code modification:
+when user click on share img svg i'll check the screen size and use the correct layout
+
+so all func call should be in the click even for the svg image
+*/
+console.log('\n');
+
+shareSvgImg.addEventListener("click",()=>{
+  console.log('checking screen size... \n' +'its ' +  screenWidth + 'px');
+  handleResize();
   
+  //pass the vw of the user as arg to determineScreenSize func, will apply correct layout based on true/false
   if(!determineScreenSize(screenWidth)){
-    console.log(`your screen is small it is currently ${screenWidth}px \napplying mobile layout styles`);
-    
-    shareSvgImg.addEventListener("click",(e)=>{
-      console.log(`You clicked on this element: ${e.target.outerHTML}`,shareSvgImg);
-      if(!jsHiddenContainer.classList.contains('hidden')){
-          jsHiddenContainer.classList.add('hidden') // when class hidden is on it means the element is hidden
-          jsHiddenMobile.classList.remove('hidden') // remove hidden class so the element will now appear on the webpage
-      }
-     
-      else{
-        // if the element has the class remove it, the hiddencontainer and its content appears on the webpage
-        jsHiddenContainer.classList.remove('hidden'); 
-      
-      }
-     
-    })
-  
-    jsShareIcon2.addEventListener('click',()=>{
-      // when the second share img is clicked re introduce the old layout and remove the mobile active layout by adding hidden class
-      /*when 2nd share svg img is clicked must remove hidden class on container so old layout can re appear and new one disappears */
-      jsHiddenContainer.classList.remove('hidden') 
-      jsHiddenMobile.classList.add('hidden')
-    })
+    console.log('mobile device detected...');
+    if(!jsHiddenContainer.classList.contains('hidden')){
+      jsHiddenContainer.classList.add('hidden') // when class hidden is on it means the element is hidden
+      jsHiddenMobile.classList.remove('hidden') // remove hidden class so the element will now appear on the webpage
   }
-
-
-
-  //for pc layout 
   else{
-    console.log(`your screen is big it is currently ${screenWidth}px  \napplying desktop layout styles`);
-     shareSvgImg.addEventListener('click',(e)=>{
-      console.log(`You clicked on this element: ${e.target.outerHTML}`,shareSvgImg);
-    })
-  }
+    // if the element has the class remove it, the hiddencontainer and its content appears on the webpage
+    jsHiddenContainer.classList.remove('hidden'); 
   
-}
+  }
+  jsShareIcon2.addEventListener('click',()=>{
+    // when the second share img is clicked re introduce the old layout and remove the mobile active layout by adding hidden class
+    /*when 2nd share svg img is clicked must remove hidden class on container so old layout can re appear and new one disappears */
+    jsHiddenContainer.classList.remove('hidden') 
+    jsHiddenMobile.classList.add('hidden')
+  })
+    
+  }
 
-acrticlePreviewActiveState();
+
+  /*pc layout */
+  else{
+    console.log('\n pc master race layout...');
+    
+  }
+})
